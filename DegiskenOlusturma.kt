0a1e6832bc941b6ebda9ae9e3ea84e82d4a93d43
yusufkrcbn <83416978+yusufkrcbn@users.noreply.github.com>
2025-04-26 21:14:19 +0300

    Create DegiskenOlusturma.kt

---
+++ DegiskenOlusturma.kt
@@ -0,0 +1,58 @@
+package com.example.kotlin.degiskenler
+
+fun main(){
+
+    var yusuf = "yusuf28"
+    val number =11.78
+    var city = "İSTANBUL"
+    val country ="Turkey"
+    var number = "05530838001"
+    val code =90
+    var email = "@gmail.com"
+    val job ="student"
+    var stock = 50
+    val consultantName ="Ahmet"
+    var amount = 100
+    val dateOfBirth ="22.10.2000"
+    var sale = 1000
+    val isMarried =true
+    var comment = "Nice"
+    val dateOfPay ="10.10.2020"
+    var pay = true
+    val numberOfOrder 12
+    var carModel = "3.20i"
+    val bookName ="Nutuk"
+    var dateOfPublish = "24.12.2024"
+    val salesAmount =30
+    var numberOfRooms = 3
+    val latitude =12
+    var longtitude = 120
+    val ProductName ="Computer"
+    var foodPrice = 100
+    val company="Nike"
+    var songName = "I was made for loving you"
+    val lengthOfVideo =120
+    var pointOfProduct = 5
+    val imageName ="image"
+    var fileFormat = ".xml"
+    val color ="black"
+    var colorCode = "#0000000"
+    val phoneModel ="Iphone"
+    var screenSize = 6.1
+    val weight =90
+    var nationalDay = "23 April"
+    val isHoliday =true
+    var dateOfRezervation = "10.10.2010"
+    val streetName ="Gul"
+    var busLine = "17K"
+    val remaininMinute =5
+    var trackingCode = 129138413897
+    val couponPeriod =10
+    var couponCode = "KAZAN10"
+    val billingAdres ="İstanbul"
+
+
+
+
+
+}
